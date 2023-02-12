685cc734bd07de77549c8f83ed80d97d8e88e1f8
Rahul Verma <extreme@Rahuls-MacBook-Air.local>
2023-02-12 16:47:47 +0530

    added signout

--- routes/user.js
+++ routes/user.js
@@ -6,9 +6,9 @@ const passport = require('../config/passport_local_strategy');
 
 router.get('/sign-in', passport.redirectProfile, userController.signIn);
 router.get('/sign-up', passport.redirectProfile ,userController.signUp);
-router.get('/sign-out', userController.signOut);
+router.get('/sign-out', userController.destroySession);
 router.get('/profile', passport.checkAuthentication,userController.profile);
 router.post('/add', userController.add);
-router.post('/authenticate', passport.authenticate('local', { failureRedirect: '/user/sign-in' }), userController.autheticate); // using passport middleware to authenticate
+router.post('/authenticate', passport.authenticate('local', { failureRedirect: '/user/sign-in' }), userController.createSession); // using passport middleware to authenticate
 
 module.exports = router;
